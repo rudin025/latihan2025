0b14ce7ff29291d28eba62e2f4d486794f02b414
hamam <adiwaetks3@gmail.com>
2025-05-07 14:22:20 +0700

    menambahkan header

--- welcome.php
+++ welcome.php
@@ -2,6 +2,12 @@
   <head>
   </head>
   <body>
-    <h1>Welcome!</h1>
+   <h1>Situs Milik Ku</h1>
+   <hr>
+     <a href="welcome.php">Welcome</a>
+	 <a href="Profil.php">Profil</a>
+	 <a href="Portofolio.php">portofolio</a>
+	 <hr>
+    <h1>Halaman Profil!</h1>
   </body>
 </html>
